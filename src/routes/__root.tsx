e95927c605005c0730bae6e62361971d25e5a7c7
Gdev-z <gabriezanon.dev@gmail.com>
2026-05-16 11:40:12 -0300

    force

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,7 +28,7 @@ function NotFoundComponent() {
 
 export const Route = createRootRoute({
   head: () => ({
-    meta: [
+    meta: [ 
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { title: "Lovable App" },
